930c8417898be6877b9086d3c895c4383cb654c8
Tommy <zhatao1992@163.com>
2019-03-30 12:18:10 +0800

    完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// eslint-disable-next-line 
+console.log('当前环境变量process.env.NODE_ENV：' + process.env.NODE_ENV)
+// .env.development 的优先级别 .env 高
+console.log(`.env中的环境变量VUE_APP_SECRET：` + process.env.VUE_APP_SECRET)
+
+console.log('VUE_APP_TITLE: ' + process.env.VUE_APP_TITLE)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    outputDir: 'dist', // 默认值，build文件的目录
+    // indexPath: 'config/index.html', // index.html的输出路径，相对于 outputDir
+}
