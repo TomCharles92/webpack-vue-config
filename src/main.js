9572dff02bdb7900751bce2ffb793651aeba310f
Tommy <zhatao1992@163.com>
2019-03-30 14:16:27 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 只有以 VUE_APP_ 开头的变量会被 webpack.DefinePlugin 静态嵌入到客户端侧的包中
+// 除了 VUE_APP_* 变量之外，在你的应用代码中始终可用的还有两个特殊的变量：
+// 1. NODE_ENV: "development" | "production" | "test"
+// 2. BASE_URL = vue.config.js => publicPath
+
 // eslint-disable-next-line 
 console.log('当前环境变量process.env.NODE_ENV：' + process.env.NODE_ENV)
 // .env.development 的优先级别 .env 高
